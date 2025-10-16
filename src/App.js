b96c59e0e38370dca3d7c9042ef2735e1cafc40d
FranciscaRuzVergara <fra.ruzv@duocuc.cl>
2025-10-15 22:57:59 -0300

    Pagina Login y modificacion de App

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Productos from "./pages/Productos";
+import Login from "./pages/Login";
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="productos" element={<Productos />} />
+          <Route path="login" element={<Login />} />
         </Route>
       </Routes>
     </BrowserRouter>
